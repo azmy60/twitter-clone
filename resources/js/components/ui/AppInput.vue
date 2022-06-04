973ec477aab37ae40ee4f10097126b1f9201fa72
azmy60 <mazmiagus2003@gmail.com>
2022-06-04 15:06:11 +0700

    update AppInput

--- resources/js/components/ui/AppInput.vue
+++ resources/js/components/ui/AppInput.vue
@@ -1,17 +1,44 @@
 <script setup lang="ts">
-defineProps<{
+import { ref } from "vue";
+
+const el = ref<HTMLInputElement | null>(null);
+
+const props = defineProps<{
     modelValue: string;
+    error?: boolean;
+    debounce?: boolean;
 }>();
 
-defineEmits<{
+const emit = defineEmits<{
     (e: "update:modelValue", value: string): void;
 }>();
+
+let debounceTimeout: NodeJS.Timeout | null = null;
+
+function updateValue() {
+    if (!props.debounce) {
+        return emitUpdateModelValue();
+    }
+
+    if (debounceTimeout) clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(emitUpdateModelValue, 500);
+}
+
+function emitUpdateModelValue() {
+    emit("update:modelValue", (el.value as HTMLInputElement).value);
+}
 </script>
 
 <template>
     <input
         :value="modelValue"
-        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
-        class="bg-transparent border-neutral-700 rounded py-4 pl-2 placeholder:text-neutral-500 disabled:bg-neutral-900 disabled:border-transparent disabled:text-neutral-600"
+        @input="updateValue"
+        class="w-full bg-transparent border rounded py-4 pl-2 placeholder:text-neutral-500 disabled:bg-neutral-900 disabled:border-transparent disabled:text-neutral-600"
+        :class="{
+            'border-neutral-700': !error,
+            'border-red-600 focus-within:outline-none focus-within:ring-1 focus-within:ring-red-600':
+                error,
+        }"
+        ref="el"
     />
 </template>
---
+++ resources/js/components/ui/AppInputWrapper.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    error?: string;
+}>();
+</script>
+
+<template>
+    <div>
+        <slot />
+        <p v-if="error" class="ml-2 text-sm text-red-600">{{ error }}</p>
+    </div>
+</template>
